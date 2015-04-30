82c427dcb074bab1dc0c2a6ff208d8271d9710a9
Andrei Karzhou <karzhouandrew@gmail.com>
2015-04-30 19:41:57 +0200

    Hello world task

--- korzhov/src/lab1/HelloWorld.java
+++ korzhov/src/lab1/HelloWorld.java
@@ -3,6 +3,6 @@ package lab1;
 //First task
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world");
     }
 }
