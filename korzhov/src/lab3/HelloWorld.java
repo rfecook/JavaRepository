8de343caf2d46a04864ce4a5cf2e7aa55e215ed8
Andrei Karzhou <karzhouandrew@gmail.com>
2015-05-02 11:36:21 +0200

    Examples

---
+++ korzhov/src/lab3/HelloWorld.java
@@ -0,0 +1,11 @@
+package lab3;
+
+import lab1.Calculator;
+
+//First task
+public class HelloWorld {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.add(2, 3);
+    }
+}
